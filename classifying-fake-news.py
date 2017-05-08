33576770ec9bf2d0e97b3d54086842758e99cce3
Alice Szeliga <aszeliga@g.hmc.edu>
2017-05-07 17:09:01 -0700

    final commit, data files renamed

--- classifying-fake-news.py
+++ classifying-fake-news.py
@@ -20,23 +20,17 @@ import nltk
 from util import *
 from cluster import *
 
-<<<<<<< HEAD
 from sklearn import cluster
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.linear_model.tests.test_ridge import ind
-=======
-from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
->>>>>>> 535fb32a54c37ff6116d606ffec9eaeb97a4173b
 
-def extract_feature_vectors(X, text_fields, numerical_fields):
+def extract_dictionary(field, X):
     """
-    Extracts a feature matrix from X, treating data as text (processed as a bag
-    of words), categorical (left as a single string), or numerical (converted
-    to float).
+    Given a filename, reads the text file and builds a dictionary of unique
+    words/punctuations.
     
     Parameters
     --------------------
-<<<<<<< HEAD
         field    -- int, index of field to extract a dictionary for
         X        -- ndarray of dimensions (n,d), data samples
     
@@ -81,8 +75,6 @@ def extract_feature_vectors(X, text_fields, numerical_fields, max_features):
     
     Parameters
     --------------------
-=======
->>>>>>> 535fb32a54c37ff6116d606ffec9eaeb97a4173b
         X                -- ndarray of dimensions (n,d), data samples
         text_fields      -- list of indices corresponding to textual fields
                             (columns in X)
@@ -98,33 +90,20 @@ def extract_feature_vectors(X, text_fields, numerical_fields, max_features):
         vectorizer     -- the CountVectorizer used
         transformer    -- the TfidfTransformer used
     """
-<<<<<<< HEAD
     n,d = X.shape
     vectorizers = {}
     transformers = {}
 
     for i in range(d):
-        vectorizers[i] = CountVectorizer(stop_words='english', lowercase=False, min_df=1, max_features=max_features)
+        vectorizers[i] = CountVectorizer(stop_words='english', lowercase=False, min_df=1, max_df = 0.8, max_features=max_features)
         transformers[i] = TfidfTransformer(use_idf=True, smooth_idf=False)
     
-=======
-    vectorizer = CountVectorizer(stop_words='english', lowercase=False, min_df=1, max_features=None)
-    transformer = TfidfTransformer(use_idf=True, smooth_idf=False)
-    
-    n,d = X.shape
->>>>>>> 535fb32a54c37ff6116d606ffec9eaeb97a4173b
     feature_matrix = np.empty((n, 0), dtype=object)
     for field in range(d):
         if field in text_fields:
             field_text = X[:, field]
-<<<<<<< HEAD
             counts = vectorizers[field].fit_transform(field_text)
             tfidf = transformers[field].fit_transform(counts)
-=======
-            counts = vectorizer.fit_transform(field_text)
-            tfidf = transformer.fit_transform(counts)
-            print "tfidf:", tfidf.toarray()
->>>>>>> 535fb32a54c37ff6116d606ffec9eaeb97a4173b
             feature_matrix = np.concatenate((feature_matrix, tfidf.toarray()), axis=1)
             
         else:
@@ -133,17 +112,10 @@ def extract_feature_vectors(X, text_fields, numerical_fields, max_features):
                 field_values = field_values.astype(np.float)
             feature_matrix = np.concatenate((feature_matrix, field_values.reshape(n, 1)), axis=1)
             
-<<<<<<< HEAD
     return feature_matrix, vectorizers, transformers
 
 
 def calculate_purity(clusters, true_labels, num_clusters, weighted=False):
-=======
-    return feature_matrix, vectorizer, transformer
-
-
-def calculate_purity(clusters, weighted=False):
->>>>>>> 535fb32a54c37ff6116d606ffec9eaeb97a4173b
     """
     Computes the proportion of the dominant class in each cluster and averages
     this across all clusters.
@@ -179,23 +151,18 @@ def calculate_purity(clusters, weighted=False):
         cluster = clusters.labels_[i]
         label = true_labels[i]
         label_index = unique_labels[label]
-        print i
-        print "actual cluster labels: ", clusters.labels_
-        print "num_unique_labels ", num_unique_labels, " num_clusters ", num_clusters, " num points ", num_points
-        print "cluster ", cluster, " label_index ", label_index
-        print "true_labels ", true_labels, "\n"
         cluster_label_counts[cluster][label_index] += 1
     
     overall_purity = 0
     for label_count in cluster_label_counts:
         dominant_class_count = max(label_count)
         total_count = sum(label_count)
-        purity = dominant_class_count / total_count
+        purity = dominant_class_count / float(total_count)
         
         if weighted:
-            weight = total_count / num_points
+            weight = total_count / float(num_points)
         else:
-            weight = 1 / num_clusters
+            weight = 1 / float(num_clusters)
         overall_purity += weight * purity
 
     return overall_purity
@@ -251,25 +218,26 @@ def main():
     # The first line of the tsv is field headers.
 
     # only run this once. afterwards, can load in X and y easily
-    """
+    #"""
     print "start"
-    data = load_data2("fake_subset_no_blanks_15.txt")
+    data = load_data2("fake_subset_no_blanks_16.txt")
     print "done loading data"
     
     headers = np.append(data.X[0, :], [data.y[0]])
     X, y = data.X[1:, :], data.y[1:]
-    np.save("labels", y)
-    np.save("values", X)
-    np.save("headers", headers)
+    # can't save on knuth
+    #np.save("labels", y)
+    #np.save("values", X)
+    #np.save("headers", headers)
+    print "done importing data"
     """
     y = np.load("labels.npy")
     X = np.load("values.npy")
     headers = np.load("headers.npy")
     print "done loading from files"
-    
+    """
     n, d = X.shape
     
-<<<<<<< HEAD
     # Defining the text vs. categorical vs numerical fields for our d
     # title (1), text (2), and thread title (5)
     text_fields = [1,2,5]
@@ -277,16 +245,6 @@ def main():
     categorical_fields = [0,3,4] 
     # spam score (6), replies count (7), participants (8), likes (9), comments (10), shares (11)
     numerical_fields = [6,7,8,9,10,11] 
-=======
-    # title (1), text (2), and thread title (5)
-    text_fields = [2] # [1,2,5]
-    
-    # author (0), site url (3), country (4)
-    categorical_fields = [0] #[0,3,4] 
-    
-    # spam score (6), replies count (7), participants (8), likes (9), comments (10), shares (11)
-    numerical_fields = [1] #[6,7,8,9,10,11] 
->>>>>>> 535fb32a54c37ff6116d606ffec9eaeb97a4173b
     
     ###################
     # PROCESSING TEXT #
@@ -303,7 +261,6 @@ def main():
     kmeans_feature_matrix, vectorizer, transformer = extract_feature_vectors(X, kmeans_text_fields, numerical_fields, max_features = max_features)
     print "done extracting feature vectors for k means"
 
-    
     # indices are shifted in the new features matrix because text columns get more features
     # we calculate the new indices for the original categorical indices
     categorical_indices_dict = {}
@@ -318,51 +275,14 @@ def main():
                 categorical_indices_dict[field] = field_index
             field_index += 1
     categorical_indices = categorical_indices_dict.values()
-
+    
+    ###################
+    # HYPERPARAMETERS #
+    ###################
     # use the elbow method to determine the number of clusters
-    num_clusters_range = range(3, 6) #range(2, 15)
-    
-    print "*********** K PROTOTYPES ***********"
-    cluster_sets = {}
-    cluster_impurities = {}
-    cluster_costs = {}
-    print "beginning elbow method to determine best # clusters"
-    for i in num_clusters_range:
-        print "  current number clusters is: ", i
-        try:
-            model = kprototypes.KPrototypes(n_clusters=i, init='Cao', verbose=2, n_init = 1, max_iter = 1)
-            print "fitting"
-            model.fit_predict(feature_matrix, categorical=categorical_indices)
-            cluster_sets[i] = model
-            # (same as 1 - purity since purity is a percentage)
-            cluster_impurities[i] = 1 - calculate_purity(model, y, i)
-            cluster_costs[i] = model.cost_
-            print "for ", i, " clusters we have impurity of ", cluster_impurities[i], " and a cost of ", cluster_costs[i]
-        except:
-            raise
-            print "    we got an error!!"
-            print "    ", i, "clusters is probably too many clusters"
-            break
-    
-    # see what we get by minimizing 
-    best_num_clusters_by_costs = calculate_best_num_clusters(cluster_costs)
-    best_num_cluster_by_impurities = calculate_best_num_clusters(cluster_impurities)
-    print "FOR K PROTOTYPES"
-    print "The best number of clusters (minimizing cost) is ", best_num_clusters_by_costs
-    print "The best number of clusters (minimizing impurity) is ", best_num_cluster_by_impurities
-    
-    print "trying to save the k-prototypes data"
-    try:
-        np.save("kp_cluster_sets_keys", cluster_sets.keys())
-        np.save("kp_cluster_sets_values", cluster_sets.values())
-        np.save("kp_cluster_impurities_keys", cluster_impurities.keys())
-        np.save("kp_cluster_impurities_values", cluster_impurities.values())  
-        np.save("kp_cluster_costs_keys", cluster_costs.keys())
-        np.save("kp_cluster_costs_values", cluster_costs.values())
-    except:
-        print "it failed. lololol this shit is probably too big lol"
-        
-        
+    """
+    num_clusters_range = range(2, 8)
+   
     print "*********** K MEANS ***********"
     k_means_cluster_sets = {}
     k_means_cluster_impurities = {}
@@ -371,7 +291,7 @@ def main():
     for i in num_clusters_range:
         print "  current number clusters is: ", i
         try:
-            kmeans_model = cluster.KMeans(n_clusters = i, verbose = 1)
+            kmeans_model = cluster.KMeans(n_clusters = i)
             print "fitting"
             kmeans_model.fit_predict(kmeans_feature_matrix)
             k_means_cluster_sets[i] = kmeans_model
@@ -392,35 +312,141 @@ def main():
     print "FOR K MEANS"
     print "The best number of clusters (minimizing cost) is ", best_num_clusters_by_costs
     print "The best number of clusters (minimizing impurity) is ", best_num_cluster_by_impurities
+    
+    print "*********** K PROTOTYPES ***********"
+    cluster_sets = {}
+    cluster_impurities = {}
+    cluster_costs = {}
+    print "beginning elbow method to determine best # clusters"
+    for i in num_clusters_range:
+        print "  current number clusters is: ", i
+        try:
+            model = kprototypes.KPrototypes(n_clusters=i, init='Cao')
+            print "fitting"
+            model.fit_predict(feature_matrix, categorical=categorical_indices)
+            cluster_sets[i] = model
+            # (same as 1 - purity since purity is a percentage)
+            cluster_impurities[i] = 1 - calculate_purity(model, y, i)
+            cluster_costs[i] = model.cost_
+            print "for ", i, " clusters we have impurity of ", cluster_impurities[i], " and a cost of ", cluster_costs[i]
+        except:
+            raise
+            print "    we got an error!!"
+            print "    ", i, "clusters is probably too many clusters"
+            break
+    
+    # see what we get by minimizing 
+    best_num_clusters_by_costs = calculate_best_num_clusters(cluster_costs)
+    best_num_cluster_by_impurities = calculate_best_num_clusters(cluster_impurities)
+    print "FOR K PROTOTYPES"
+    print "The best number of clusters (minimizing cost) is ", best_num_clusters_by_costs
+    print "The best number of clusters (minimizing impurity) is ", best_num_cluster_by_impurities
+     
+    ## looking at different values of gamma
+    gamma_range = [0.01, 0.1, 1, 10, 100]
+    clusters = 4
+    cluster_sets = {}
+    cluster_impurities = {}
+    cluster_costs = {}
+    print "examining different gamma values for k prototypes"
+    for i in gamma_range:
+        print "  current gamma value is: ", i
+        try:
+            model = kprototypes.KPrototypes(n_clusters=clusters, init='Cao', gamma = i, verbose=2)
+            print "fitting"
+            model.fit_predict(feature_matrix, categorical=categorical_indices)
+            cluster_sets[i] = model
+            # (same as 1 - purity since purity is a percentage)
+            cluster_impurities[i] = 1 - calculate_purity(model, y, clusters)
+            cluster_costs[i] = model.cost_
+            print "for gamma = ", i, " we have impurity of ", cluster_impurities[i], " and a cost of ", cluster_costs[i]
+        except:
+            raise
+            print "    we got an error!!"
+            break
+    
+    # see what we get by minimizing 
+    best_num_clusters_by_costs = calculate_best_num_clusters(cluster_costs)
+    best_num_cluster_by_impurities = calculate_best_num_clusters(cluster_impurities)
+    print "FOR K PROTOTYPES"
+    print "The best number of clusters (minimizing cost) is ", best_num_clusters_by_costs
+    print "The best number of clusters (minimizing impurity) is ", best_num_cluster_by_impurities
+    """
+    
+    ###################
+    #    ANALYSIS     #
+    ###################
+    num_clusters = 4
+    g = 10
+    # These are our final models
+    k_proto_model = kprototypes.KPrototypes(n_clusters = num_clusters, init='Cao', gamma = g)##, verbose=2)
+    print "fitting"
+    k_proto_labels = k_proto_model.fit_predict(feature_matrix, categorical=categorical_indices)
 
-    print "trying to save the k-means data"
-    try:
-        np.save("km_cluster_sets_keys", k_means_cluster_sets.keys())
-        np.save("km_cluster_sets_values", k_means_cluster_sets.values())
-        np.save("km_cluster_impurities_keys", k_means_cluster_impurities.keys())
-        np.save("km_cluster_impurities_values", k_means_cluster_impurities.values())  
-        np.save("km_cluster_costs_keys", k_means_cluster_costs.keys())
-        np.save("km_cluster_costs_values", k_means_cluster_costs.values())
-    except:
-        print "it failed. lololol this shit is probably too big lol"
+    k_means_model = cluster.KMeans(n_clusters = num_clusters)
+    print "fitting"
+    k_means_labels = k_means_model.fit_predict(kmeans_feature_matrix)
+    
+    
+    # get values for all n clusters
+    labels = np.unique(k_proto_labels)
+    k_proto_ind = [[] for x in range(num_clusters)]
+    k_means_ind = [[] for x in range(num_clusters)]
+    for i in range(n):
+        # find which cluster this example maps to for k prototypes
+        j = k_proto_labels[i] - 1
+        k_proto_ind[j].append(i)
+        # find which cluster this example maps to for k means
+        j = k_means_labels[i] - 1
+        k_means_ind[j].append(i)
 
+    k_proto_counts = [len(x) for x in k_proto_ind]
+    k_means_counts = [len(x) for x in k_means_ind]
 
+    print "k prototypes"
+    print "The cluster sizes are ", k_proto_counts
+    print "k means"
+    print "The cluster sizes are",  k_means_counts
+        
+    # getting actual rows
+    k_proto_clusters = {}
+    k_means_clusters = {}
+    k_proto_y = {}
+    k_means_y = {}
+    for i in range(num_clusters):
+        k_proto_clusters[i] = X[k_proto_ind[i], :]
+        k_means_clusters[i] = X[k_means_ind[i], :]
+        k_proto_y[i] = y[k_proto_ind[i]]
+        k_means_y[i] = y[k_proto_ind[i]]
+    
+    k_proto_y_labels = {}
+    k_means_y_labels = {}
+    y_labels = np.unique(y)
+    for i in range(num_clusters):
+        #for label in y_labels:
+        print k_proto_y[i]
+        print [np.count_nonzero(k_proto_y[i] == label) for label in y_labels]
+        k_proto_y_labels[i][label] = [np.count_nonzero(k_proto_y[i] == label) for label in y_labels]
+        k_means_y_labels[i][label] = [np.count_nonzero(k_means_y[i] == label) for label in y_labels]
+    print k_proto_y_labels[1]
+    """
+    for i in range(num_clusters):
+        print "k prototypes"
+        print "  for cluster ", i, " the labels are "
+        kp_unique, kp_counts = np.unique(k_proto_y_labels[i], return_counts = True)
+        print "  ", dict(zip(kp_unique, kp_counts))
+    for i in range(num_clusters):
+        print "k means"
+        print "  for cluster ", i, " the labels are "
+        km_unique, km_counts = np.unique(k_means_y_labels[i], return_counts = True)
+        print "  ", dict(zip(km_unique, km_counts))
+    """
 
 
     
-<<<<<<< HEAD
-=======
-    feature_matrix, vectorizer, transformer = extract_feature_vectors(X, text_fields, numerical_fields)
+    return
 
-    print X
-    print feature_matrix
     
-    #model = kprototypes.KPrototypes(n_clusters=2, init='Cao', verbose=2)
-    #clusters = model.fit_predict(X, categorical=categorical_indices)
-    #print clusters
-
-            
->>>>>>> 535fb32a54c37ff6116d606ffec9eaeb97a4173b
 if __name__ == "__main__" :
     main()
     
